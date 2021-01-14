7b6d8200a591ae6d8c5cdd1a4ee102e784ddd3a7
kimsehwa0320 <75105345+kimsehwa0320@users.noreply.github.com>
2021-01-14 15:08:40 +0900

    up

---
+++ 02php/header.php
@@ -0,0 +1,21 @@
+<!DOCTYPE html>
+<html lang="ko">
+<head>
+	<meta charset="UTF-8">
+	<meta name="viewport" content="width=device-width, initial-scale=1.0">
+	<title>my homepage</title>
+	<link rel="stylesheet" href="css/style.css">
+</head>
+<body>
+	<header id="header">
+		<h1><a href="./">logo</a></h1> <!-- ./ -> index파일로 이동 -->
+		<nav class="gnb">
+			<ul class="clearfix">
+				<li><a href="company.html">company</a></li>
+				<li><a href="location.html">location</a></li>
+				<li><a href="#">menu3</a></li>
+				<li><a href="#">menu4</a></li>
+				<li><a href="#">menu5</a></li>
+			</ul>
+		</nav>
+	</header>
