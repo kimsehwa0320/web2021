267ec396609743237ee4b3886acb57c0109569b3
kimsehwa0320 <75105345+kimsehwa0320@users.noreply.github.com>
2021-01-22 23:33:13 +0900

    up

---
+++ 01javascript/20210122_menu/js/menu.js
@@ -0,0 +1,16 @@
+
+  function nowMenu(menu){
+    console.log(menu)
+    $("ul.menu>li").eq(menu).addClass("active");
+    //$(".bar").animate({left:menu*200}); //menu 0 1 2 3 4
+    $(".bar").css({left:menu*200}); //끊겨나옴
+
+    //$("ul.menu>li").hover(function(){},function(){});
+                           //mouse enter  mouse Leave
+    $("ul.menu>li").hover(function(){
+      var menuLi = $(this).index();
+      $(".bar").stop().animate({left:menuLi*200},50);
+    },function(){
+      $(".bar").stop().animate({left:menu*200},50);
+    });
+  }
