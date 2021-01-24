a75a620a52e06a176ed1062b5668433fe5f93efd
kimsehwa0320 <75105345+kimsehwa0320@users.noreply.github.com>
2021-01-25 08:47:14 +0900

    up

--- 20210111_company/js/sub_menu.js
+++ 20210111_company/js/sub_menu.js
@@ -2,15 +2,5 @@
   function nowMenu(menu){
     console.log(menu)
     $("ul.menu>li").eq(menu).addClass("active");
-    //$(".bar").animate({left:menu*200}); //menu 0 1 2 3 4
-    $(".bar").css({left:menu*90}); //끊겨나옴
 
-    //$("ul.menu>li").hover(function(){},function(){});
-                           //mouse enter  mouse Leave
-    // $("ul.menu>li").hover(function(){
-    //   var menuLi = $(this).index();
-    //   $(".bar").stop().animate({left:menuLi*90},50);
-    // },function(){
-    //   $(".bar").stop().animate({left:menu*90},50);
-    // });
   }
